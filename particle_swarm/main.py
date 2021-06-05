ec0721a23a8d56accdb2a7d8f8a437e7c698770a
KrzysiekJa <kjmj77@gmail.com>
2021-06-05 15:41:55 +0200

    Added visualizations in main.py

--- particle_swarm/main.py
+++ particle_swarm/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from optimalizator.PSO import PSO
 
 
@@ -15,3 +16,14 @@ if __name__ == "__main__":
     print("\n", pso.p_best)
     print(pso.p_best_vals)
     print(pso.particles)
+    
+    
+    x = np.arange(-5, 5, 0.1); y = np.arange(-5, 5, 0.1)
+    xx, yy = np.meshgrid(x, y, sparse=True)
+    z = function(xx, yy)
+    
+    plt.contourf(x,y,z, 20, cmap=plt.cm.ocean, origin='lower')
+    plt.plot(pso.particles[0], pso.particles[1], 'rx', markersize=3)
+    plt.plot(pso.p_best[0], pso.p_best[1], 'bo', markersize=3)
+    plt.plot(pso.g_best[0], pso.g_best[1], 'mo', markersize=3)
+    plt.show()
