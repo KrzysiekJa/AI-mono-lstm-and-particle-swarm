a36507b24f0da8e8d76a5abcb7bfa218502721bd
KrzysiekKlim <krzysiek.klim1999@gmail.com>
2021-06-07 16:01:41 +0200

    Add 3d visualization

--- particle_swarm/main.py
+++ particle_swarm/main.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 from optimalizator.PSO import PSO
@@ -24,22 +26,23 @@ if __name__ == "__main__":
     # y_range = [-5, 5]
     # pso = PSO(function, 30, [x_range[0], x_range[1]], [y_range[0], y_range[1]], 0.5, 2, 2, 30)
 
-    f_name = "Eggholder function"
-    function = lambda x, y: -(y + 47) * np.sin(np.sqrt(np.abs(x / 2 + (y + 47)))) - x * np.sin(
-        np.sqrt(np.abs(x - (y + 47))))
-    x_range = [-520, 520]
-    y_range = [-520, 520]
-    pso = PSO(function, 30, [x_range[0], x_range[1]], [y_range[0], y_range[1]], 0.5, 0.2, 0.2, 30)
+    # f_name = "Eggholder function"
+    # function = lambda x, y: -(y + 47) * np.sin(np.sqrt(np.abs(x / 2 + (y + 47)))) - x * np.sin(
+    #     np.sqrt(np.abs(x - (y + 47))))
+    # x_range = [-520, 520]
+    # y_range = [-520, 520]
+    # pso = PSO(function, 30, [x_range[0], x_range[1]], [y_range[0], y_range[1]], 0.5, 0.2, 0.2, 30)
 
-    # f_name = "Hölder table function"
-    # function = lambda x, y: -np.abs(
-    #     np.sin(x) * np.cos(y) * expit(np.abs(1 - (np.sqrt(np.power(x, 2) + np.power(y, 2)) / np.pi))))
-    # x_range = [-5, 5]
-    # y_range = [-5, 5]
-    # pso = PSO(function, 30, [x_range[0], x_range[1]], [y_range[0], y_range[1]], 0.1, 0.01, 0.01, 3000)
+    f_name = "Hölder table function"
+    function = lambda x, y: -np.abs(
+        np.sin(x) * np.cos(y) * np.exp(np.abs(1 - (np.sqrt(np.power(x, 2) + np.power(y, 2)) / np.pi))))
+    x_range = [-10, 10]
+    y_range = [-10, 10]
+    pso = PSO(function, 10, [x_range[0], x_range[1]], [y_range[0], y_range[1]], 0.1, 0.01, 0.01, 3000)
 
+    step = 0.05
 
-    step = 0.5
+    # chart before opt
     x = np.arange(x_range[0], x_range[1], step)
     y = np.arange(y_range[0], y_range[1], step)
     xx, yy = np.meshgrid(x, y, sparse=True)
@@ -47,6 +50,8 @@ if __name__ == "__main__":
     plt.contourf(x, y, z, 20, cmap=plt.cm.ocean, origin='lower')
     plt.plot(pso.particles[0], pso.particles[1], 'rx', markersize=3)
     plt.title("Before optimization")
+    plt.xlabel('x')
+    plt.ylabel('y')
     plt.show()
 
     pso.compute()
@@ -55,14 +60,23 @@ if __name__ == "__main__":
     # print(pso.p_best_vals)
     # print(pso.particles)
 
+    # chart after opt
     plt.contourf(x, y, z, 20, cmap=plt.cm.ocean, origin='lower')
-    plt.plot(pso.particles[0], pso.particles[1], 'rx', markersize=3)
-    plt.plot(pso.p_best[0], pso.p_best[1], 'bo', markersize=3)
+    #plt.plot(pso.particles[0], pso.particles[1], 'rx', markersize=3)
+    plt.plot(pso.p_best[0], pso.p_best[1], 'rx', markersize=3)
     plt.plot(pso.g_best[0], pso.g_best[1], 'yo', markersize=3)
     plt.title(
         f'After optimization\nf({round(pso.g_best[0], 5)}, {round(pso.g_best[1], 5)}) = {round(function(pso.g_best[0], pso.g_best[1]), 5)}')
+    plt.xlabel('x')
+    plt.ylabel('y')
     plt.show()
 
-    plt.contourf(x, y, z, 20, cmap=plt.cm.ocean, origin='lower')
+    # 3D chart
+    ax = plt.axes(projection='3d')
+    ax.contour3D(x, y, z, 50, cmap=plt.cm.ocean)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    ax.view_init(25, 35)
     plt.title(f_name)
     plt.show()
